d0f1bc33d7c61f647353a5bec08c88b06354b86d
Pierre Lorenzi <lorenzi.online@gmail.com>
2024-07-26 00:13:13 +0200

    Graph

--- src/main/java/com/plo/alceste/graph/Graph.java
+++ src/main/java/com/plo/alceste/graph/Graph.java
@@ -1,11 +1,10 @@
 package com.plo.alceste.graph;
 
-import com.plo.alceste.model.ComparisonLink;
-import com.plo.alceste.model.GraphElement;
-import com.plo.alceste.model.Vertex;
+import com.plo.alceste.model.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 public record Graph(List<GraphElement> elements) {
 
@@ -13,18 +12,66 @@ public record Graph(List<GraphElement> elements) {
         this(new ArrayList<>());
     }
 
-    public ComparisonLink findLoopAround(Vertex vertex) {
-        List<ComparisonLink> loops = elements.stream()
-                .filter(e -> e instanceof ComparisonLink)
-                .map(e -> (ComparisonLink) e)
-                .filter(link -> link.getSource() == vertex && link.getDestination() == vertex)
+    public Relationship findRelationshipBetween(Vertex v1, Vertex v2) {
+
+        Dependency dependency = findDependencyBetween(v1, v2);
+        List<ComparisonLink> comparisonLinks = findComparisonsBetween(v1, v2);
+        List<ComparisonLink> loops1 = findLoops(v1);
+        List<ComparisonLink> loops2 = findLoops(v2);
+
+
+    }
+
+    private Dependency findDependencyBetween(Vertex v1, Vertex v2) {
+        if (isAncestor(v1, v2)) {
+            return new Dependency(v1, v2);
+        }
+        else if (isAncestor(v2, v1)) {
+            return new Dependency(v2, v1);
+        }
+        else {
+            return null;
+        }
+    }
+
+    private boolean isAncestor(Vertex possibleAncestor, Vertex possibleDescendant) {
+        Stream<Vertex> ancestors = Stream.iterate(possibleDescendant, this::findParent);
+        return ancestors.anyMatch(ancestor -> ancestor == possibleAncestor);
+    }
+
+    private Vertex findParent(Vertex vertex) {
+        List<DependencyLink> links = streamElements(DependencyLink.class)
+                .filter(l -> l.getDestination() == vertex && l.getProbability() == 1.0)
                 .toList();
-        return switch (loops.size()) {
+        return switch (links.size()) {
             case 0 -> null;
-            case 1 -> loops.get(0);
-            default -> throw new RuntimeException("There are multiple loops: " + loops);
+            case 1 -> links.get(0).getOrigin();
+            default -> throw new RuntimeException("Vertex has multiple parents");
         };
     }
+
+    private <T extends GraphElement> Stream<T> streamElements(Class<T> type) {
+        return elements.stream()
+                .filter(type::isInstance)
+                .map(type::cast);
+    }
+
+    private boolean doesLinkHaveVertices(Link l, Vertex v1, Vertex v2) {
+        return (l.getOrigin() == v1 && l.getDestination() == v2)
+                || (l.getDestination() == v2 && l.getOrigin() == v1);
+    }
+
+    private List<ComparisonLink> findComparisonsBetween(Vertex v1, Vertex v2) {
+        return streamElements(ComparisonLink.class)
+                .filter(l -> doesLinkHaveVertices(l, v1, v2))
+                .toList();
+    }
+
+    private List<ComparisonLink> findLoops(Vertex vertex) {
+        return findComparisonsBetween(vertex, vertex);
+    }
+
+    public record Dependency(Vertex parent, Vertex child) {}
 
     public enum Relationship {
         DEPENDENCY,
